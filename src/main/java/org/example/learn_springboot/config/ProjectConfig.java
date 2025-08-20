e3349bd6d0aea2078bb36c5b95d89e147c999108
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-21 05:45:21 +0700

    Defining the Person and the Parrot beans in ProjectConfig

--- src/main/java/org/example/learn_springboot/config/ProjectConfig.java
+++ src/main/java/org/example/learn_springboot/config/ProjectConfig.java
@@ -1,5 +1,8 @@
 package org.example.learn_springboot.config;
 
+import org.example.learn_springboot.entity.Parrot;
+import org.example.learn_springboot.entity.Person;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +11,18 @@ import org.springframework.context.annotation.Configuration;
 //To tell Spring it needs to search for classes annotated with stereotype annotations.
 public class ProjectConfig {
 
+    @Bean
+    public Parrot parrot() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    public Person person() {
+        Person p = new Person();
+        p.setName("Ella");
+        return p;
+    }
 
 }
