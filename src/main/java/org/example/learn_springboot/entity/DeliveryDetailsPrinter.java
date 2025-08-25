32a896890046245b5b117884bf70f6a10d2f9a90
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-26 06:31:40 +0700

    move the creation part to the parameters

--- src/main/java/org/example/learn_springboot/entity/DeliveryDetailsPrinter.java
+++ src/main/java/org/example/learn_springboot/entity/DeliveryDetailsPrinter.java
@@ -1,12 +1,21 @@
 package org.example.learn_springboot.entity;
 
 public class DeliveryDetailsPrinter {
+
+    private SorterByAddress sorterByAddress;
+    private SorterByName sorterByName;
+
+    public DeliveryDetailsPrinter(SorterByAddress sorterByAddress, SorterByName sorterByName) {
+        this.sorterByAddress = sorterByAddress;
+        this.sorterByName = sorterByName;
+    }
+
     public void printDetails(String type) {
         if ("Address".equals(type)) {
-            SorterByAddress sorterByAddress = new SorterByAddress();
+            sorterByAddress = new SorterByAddress();
             sorterByAddress.sortDetails();
         } else if ("Name".equals(type)) {
-            SorterByName sorterByName = new SorterByName();
+            sorterByName = new SorterByName();
             sorterByName.sortDetails();
         } else {
             throw new RuntimeException("sorting type not supported");
@@ -15,7 +24,10 @@ public class DeliveryDetailsPrinter {
     }
 
     public static void main(String[] args) {
-        DeliveryDetailsPrinter deliveryDetailsPrinter = new DeliveryDetailsPrinter();
+        DeliveryDetailsPrinter deliveryDetailsPrinter = new DeliveryDetailsPrinter(
+                new SorterByAddress(),
+                new SorterByName()
+        );
         deliveryDetailsPrinter.printDetails("Address");
         deliveryDetailsPrinter.printDetails("Name");
     }
