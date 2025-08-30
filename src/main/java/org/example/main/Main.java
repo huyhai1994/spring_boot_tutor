10596ec43362048e5a7132f37e443baad36a7284
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-30 14:38:33 +0700

    remove code related to thread of execution and add Lazy initialization to the CommentService.java

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,45 +1,12 @@
 package org.example.main;
 
 import org.example.config.ProjectConfig;
-import org.example.service.CommentService;
-import org.example.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.Date;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) {
         var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        var commentService = c.getBean(CommentService.class);
-        int threadNumbers = 5;
-        long iteration = 100_000_000L;
-        ExecutorService executorService = Executors.newFixedThreadPool(threadNumbers);
-
-        long startTime = System.currentTimeMillis();
-
-        for (int t = 0; t < threadNumbers; t++) {
-            executorService.execute(() -> {
-                for (long i = 0; i < iteration; i++) {
-                    commentService.increaseCounter();
-                }
-            });
-        }
-        executorService.shutdown();
-        try {
-            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-
-        long endTime = System.currentTimeMillis();
-        System.out.println("executed time: " + (endTime - startTime));
-        System.out.println("Final counter value: " + commentService.getCounter());
-        System.out.println("Expected value if no race condition: " + (iteration * threadNumbers));
     }
 
 
--- src/main/java/org/example/service/CommentService.java
+++ src/main/java/org/example/service/CommentService.java
@@ -1,13 +1,16 @@
 package org.example.service;
 
 import org.example.repository.CommentRepository;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
+@Lazy
 public class CommentService {
     private final CommentRepository commentRepository;
 
     public CommentService(CommentRepository commentRepository) {
+        System.out.println("CommentService instance created!" + Thread.currentThread().getName());
         this.commentRepository = commentRepository;
     }
 
