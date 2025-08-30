23676966b461203fd238f77fb46914c3e038e262
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-30 14:38:33 +0700

    remove code related to thread of execution and add Lazy initialization to the CommentService.java

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,12 +1,16 @@
 package org.example.main;
 
 import org.example.config.ProjectConfig;
+import org.example.service.CommentService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        System.out.println("Before retrieving the CommentService!!!");
+        var service = c.getBean(CommentService.class);
+        System.out.println("after retrieving the CommentService!!!");
     }
 
 
