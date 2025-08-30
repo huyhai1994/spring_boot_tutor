83dfd7043d0fadf651abe910b68b4ad84ce2ad46
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-08-30 10:12:09 +0700

    use the static factory methods Executor

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -5,33 +5,43 @@ import org.example.service.CommentService;
 import org.example.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Date;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
         var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
         var commentService = c.getBean(CommentService.class);
-        Runnable myNewThread = () -> {
-            for (int i = 0; i < 100000000; i++) {
-                System.out.println(commentService.increaseCounter() + " " + Thread.currentThread().getName());
-            }
-            Thread.dumpStack();
-        };
-        Thread newThread1 = new Thread(myNewThread);
-        Thread newThread2 = new Thread(myNewThread);
-        Thread newThread3 = new Thread(myNewThread);
-        newThread1.start();
-        newThread2.start();
-        newThread3.start();
-        try {
-            newThread1.join();
-            newThread2.join();
-            newThread3.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        int threadNumbers = 5;
+        var iteration = 10000;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadNumbers);
+
+        long startTime = System.currentTimeMillis();
+
+        for (int t = 0; t < threadNumbers; t++) {
+            executorService.execute(() -> {
+                for (int i = 0; i < iteration; i++) {
+                    System.out.println(commentService.increaseCounter() + " " + Thread.currentThread().getName());
+                }
+            });
         }
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("executed time: " + (endTime - startTime));
         System.out.println("Final counter value: " + commentService.getCounter());
-        System.out.println("Expected value if no race condition: " + (1000 * 3));
-
-
+        System.out.println("Expected value if no race condition: " + (iteration * threadNumbers));
     }
+
+
 }
+
