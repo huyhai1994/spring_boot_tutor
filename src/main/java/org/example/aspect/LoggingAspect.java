3b64402d6c433bd1a51705f65f780e8d4637d488
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-03 05:35:42 +0700

    Step 3+4:  use an advice annotation to tell spring when and which method calls to intercept

--- src/main/java/org/example/aspect/LoggingAspect.java
+++ src/main/java/org/example/aspect/LoggingAspect.java
@@ -1,10 +1,19 @@
 package org.example.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+
+import java.util.logging.Logger;
 
 @Aspect
 public class LoggingAspect {
-    public void log() {
-        // TODO: implement later
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
+    @Around("execution(* org.example.service.*.*(..))")
+    public void log(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Method will execute");
+        joinPoint.proceed();
+        logger.info("Method executed");
     }
 }
