6c80820da4c785e0ad96713737013300d42001b8
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-03 05:21:09 +0700

    Step 2: create a class that defines the aspect, and add an instance for this class in the Spring context.

---
+++ src/main/java/org/example/aspect/LoggingAspect.java
@@ -0,0 +1,10 @@
+package org.example.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+    public void log() {
+        // TODO: implement later
+    }
+}
--- src/main/java/org/example/config/ProjectConfig.java
+++ src/main/java/org/example/config/ProjectConfig.java
@@ -1,5 +1,7 @@
 package org.example.config;
 
+import org.example.aspect.LoggingAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -9,4 +11,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy
 public class ProjectConfig {
 
+    @Bean
+    public LoggingAspect aspect() {
+        return new LoggingAspect();
+    }
 }
