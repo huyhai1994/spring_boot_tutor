0a6cff3d2c60157330e1b83f46589923630dd8ff
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-02 14:45:26 +0700

    reset project

--- src/main/java/org/example/config/ProjectConfig.java
+++ src/main/java/org/example/config/ProjectConfig.java
@@ -1,13 +1,8 @@
 package org.example.config;
 
-import org.example.service.CommentService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"org.example"})
 public class ProjectConfig {
 
 }
