0b5c8379dc2f4820aea710ebe77d635798b64b74
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-02 15:03:23 +0700

    start AOP programming

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,12 +1,19 @@
 package org.example.main;
 
 import org.example.config.ProjectConfig;
+import org.example.entity.Comment;
+import org.example.service.CommentService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var service = c.getBean(CommentService.class);
+        Comment comment = new Comment();
+        comment.setText("Demo comment");
+        comment.setAuthor("Natasha");
+        service.publishComment(comment);
     }
 }
 
---
+++ src/main/java/org/example/service/CommentService.java
@@ -0,0 +1,15 @@
+package org.example.service;
+
+import org.example.entity.Comment;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class CommentService {
+    private final Logger logger = Logger.getLogger(CommentService.class.getName());
+
+    public void publishComment(Comment comment) {
+        logger.info("Publishing comment: " + comment.getText());
+    }
+}
