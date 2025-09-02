d453b47dd5becb4ab68f399fe248420318e13c16
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-03 05:17:04 +0700

    Step 1: Enabling the aspects mechanism for you application.

--- src/main/java/org/example/config/ProjectConfig.java
+++ src/main/java/org/example/config/ProjectConfig.java
@@ -1,12 +1,12 @@
 package org.example.config;
 
-import org.example.service.CommentService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = {"org.example"})
+@EnableAspectJAutoProxy
 public class ProjectConfig {
 
 }
