0876b2ec1812479cb0155f4c7b1631609bf4cd98
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-05 06:31:12 +0700

    Specifying Aspects Ordering using the @Order Annotation

--- src/main/java/org/example/aspect/LoggingAspect.java
+++ src/main/java/org/example/aspect/LoggingAspect.java
@@ -4,10 +4,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 
 import java.util.logging.Logger;
 
 @Aspect
+@Order(2)
 public class LoggingAspect {
     private final Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
